a52c61d610c8e89f7717feb8cafbc60898dc8cca
Matheus Regis <167471928+m4th3usrpb@users.noreply.github.com>
2025-10-15 20:36:47 -0300

    Enhance welcome message in main function
    Added a welcome message about essential math resources.

---
+++ stratup.cpp
@@ -0,0 +1,13 @@
+// Simples programa de C++ que faz,obiviamente, impressão de texto//
+
+#include<iostream>//O trivial para que o programa gere saída de dados na tela//
+
+
+//função main que inicia a execução do programa//
+int main()
+{
+  std::cout << "Bem vindo ao math-pancakes!!!\n"; // mensagem //
+  std::cout << "Aqui você pode econtrar o essêncial (se possível) da matemática\n";
+
+  return 0;
+}//fim
