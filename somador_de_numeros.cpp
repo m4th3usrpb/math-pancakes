4e663a242b931014eedc3ff5b30aafabef156240
Matheus Regis <167471928+m4th3usrpb@users.noreply.github.com>
2025-11-12 20:40:01 -0300

    Improve comments and formatting in somador_de_numeros.cpp
    Refactor comments for clarity and consistency.

--- somador_de_numeros.cpp
+++ somador_de_numeros.cpp
@@ -1,18 +1,28 @@
-//Esse programa é simples,soma apenas dois números inteiros :)//
+/*************************************************************************************
+Programa: somador_de_numeros.cpp
+Descrição: Soma dois numeros fornercidos pelo usuário
+Autor: Matheus Regis
+*************************************************************************************/
 #include<iostream>
 
-//função main//
+//função main
 int main()
 {
+  //Variaveis
   int number1;
   int number2;
   int sum;
-  std::cout<<"Digite o primeiro número inteiro: ";//entrada do primeiro número//
-  std::cin>>number1;//leitura do primeiro número//
-  std::cout<<"Digite o segundo número inteiro: ";//entrada do segundo número//
-  std::cin>>number2;//leitura do segundo número//
-  sum=number1+number2;//soma dos dois números//
-  std::cout<<"A soma dos dois números é: "<<sum<<std::endl;//exibição do resultado//
+
+  //Data Entrance
   
-  return 0;//retorno do zero//
+  std::cout<<"Digite o primeiro número inteiro: ";//entrada do primeiro número
+  std::cin>>number1;//leitura do primeiro número
+  std::cout<<"Digite o segundo número inteiro: ";//entrada do segundo número
+  std::cin>>number2;//leitura do segundo número
+
+  //Soma dos números
+  sum=number1+number2;//soma dos dois números//
+  std::cout<<"A soma dos dois números é: "<<sum<<std::endl;//exibição do resultado
+  
+  return 0;
 }//fim do programa
